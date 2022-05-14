6bd67457df56ef709b3da3bb46061a5bfee94ae6
mgrout81 <mgrout81@gmail.com>
2022-05-13 17:38:52 -0700

    storyconfig add mouse dev

--- nanostringStoryConfig.js
+++ nanostringStoryConfig.js
@@ -4,7 +4,7 @@ export function getConfig() {
     return config;
 }
 
-const SUPPORTED_STORY_TYPES = ['colon', 'lymph node', 'mouse brain', 'human brain', 'kidney', 'pancreas'];
+const SUPPORTED_STORY_TYPES = ['colon', 'lymph node', 'mouse brain', 'human brain', 'kidney', 'pancreas', 'mousedev'];
 
 function loadNanostringStory(storyType) {
     const storyFile = (() => {
@@ -21,6 +21,8 @@ function loadNanostringStory(storyType) {
           return require('./nanostringKidney');
         case 'pancreas':
           return require('./nanostringPancreas');
+        case 'mousedev':
+          return require('./nanostringMouseDev');
         default:
           throw new Error('unsupported story type');
       }
